eb44cc073fa08a30de3abfc87b1f24a279072d65
Slamet Riky Riyanto <riyanriky@gmail.com>
2020-04-09 21:39:56 +0700

    edit file config

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "garuda22", "biodata");
+$mysqli = new mysqli("localhost", "", "", "");
 if ($mysqli->connect_error) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 }
